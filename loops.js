82da04448f633ca97dfcf16ba55703d8f2f7a0d5
Diego F Garcia <gadifer54@gamil>
2023-10-10 11:12:12 -0400

    loops

---
+++ loops.js
@@ -0,0 +1,48 @@
+//say hello 3 times 
+
+for (let i = 0; i < 3 ;i++){ 
+    console.log ('hello')
+}
+
+
+for(let i = 0; i < 4; i++){
+    console.log(i)
+}
+
+//made aloop that says hello to a person in the arrray
+
+const people = ['todd', 'andre', 'derek']
+
+
+
+    for (let i = 0; i < people.length; i++){
+        console.log ("hello " + people[i] ) 
+    }
+
+
+
+    const array = ['apple','grape','banana']
+
+
+    for (let i=0; i < 3 ; i++){
+        console.log(array[i])
+
+    } 
+
+
+    const array = ['apple','grape','banana']
+    for (let i = array.length - 1; i >=0; i--){
+        console.log (fruits[i])
+    }
+
+
+
+
+
+    const video = ['cats', 'news', 'games', 'sports']
+          for (let screen = 0; video.lenght < 4; screen++){
+            console.log( video + screen )
+          } 
+
+    //console log each of these videos to the screen 
+    // make sure it for any lenght or array 
---
+++ video.js
@@ -0,0 +1,5 @@
+const video = ['cats', 'news', 'games', 'sports']
+          for (let screen = 0; screen < 4; screen++){
+            console.log( video[screen] )
+
+          }
